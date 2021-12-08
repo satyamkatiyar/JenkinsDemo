b7d48e4fdf6d7030d15b50656481fb7314df9a9b
satyamkatiyar <satyam.katiyar@hotmail.com>
2021-12-08 20:37:10 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
-print('hello')
+print('*** Hello - This is Demo ****')
+print('*** Below is list of 25 test files to be executed ****')
+TestList = ["File1.jmx", "File2.jmx", "File3.jmx", "File4.jmx", "File5.jmx"]
+count = 0
+for test in TestList:
+    count += 1
+    print("Test{}: {}".format(count, test.strip()))
